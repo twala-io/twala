08aa32f1339f24e02ccab6f88727bad2012dd592
Alexander Paul Quinit <paulquinit@gmail.com>
2022-12-14 16:08:09 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,48 @@
 import GeneratorHelper from './Helpers/GeneratorHelper'
 
+type Options = {
+  apiVersion: string,
+  maxNetworkRetries: number,
+  timeout: number,
+}
+
 export class Twala {
   private appUuid: string
   private appSecret: string
-
-  constructor(appUuid: string, appSecret: string) {
-    this.appUuid = appUuid
-    this.appSecret = appSecret
+  private options: Options = {
+    apiVersion: 'v1',
+    maxNetworkRetries: 0,
+    timeout: 10000,
   }
 
-  public generateNonce() {
-    const generatorHelper = new GeneratorHelper()
-    const nonce = generatorHelper.generateNonce()
-    return nonce
+  constructor(appUuid: string, appSecret: string, options?: Options) {
+    this.appUuid = appUuid
+    this.appSecret = appSecret
+    if (options) {
+      this.options = options
+    }
+  }
+
+  // utils
+  public utils = new class {
+    public generateNonce() {
+      const generatorHelper = new GeneratorHelper()
+      const nonce = generatorHelper.generateNonce()
+      return nonce
+    }
+  }
+
+  // id
+  public id = new class {
+    public requestClaim() {
+      return 'REQUEST CLAIM'
+    }
+  }
+
+  // sign
+  public sign = new class {
+    public sendDocument() {
+      return 'SEND DOCUMENT'
+    }
   }
 }
--- tests/id.test.ts
+++ tests/id.test.ts
@@ -1,10 +1,10 @@
 import { Twala } from '../src/index'
 
-test('generate nonce', () => {
+test('request claim', () => {
   const twala = new Twala('test_app_uuid', 'test_app_secret', {
     apiVersion: 'v2',
     maxNetworkRetries: 1,
     timeout: 20000,
   })
-  expect(twala.utils.generateNonce()).toMatch(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi)
+  expect(twala.id.requestClaim()).toMatch('REQUEST CLAIM')
 })
