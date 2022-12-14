c37e5db53873c31c3d07c7f6954709cad8af7c46
Alexander Paul Quinit <paulquinit@gmail.com>
2022-12-14 20:37:41 +0800

    1.0.5

--- src/Helpers/CryptoHelper.ts
+++ src/Helpers/CryptoHelper.ts
@@ -2,7 +2,7 @@ import * as CryptoJS from 'crypto-js';
 
 export default class CryptoHelper {
   public signDataHMAC(data: string, key: string): string {
-    const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, "key");
+    const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, 'key');
     hmac.update(data);
     const hash = hmac.finalize();
     return hash.toString(CryptoJS.enc.Base64);
