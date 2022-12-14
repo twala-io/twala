5fa2b7f8322cfb7c4d172b8759d02414485dcfda
Alexander Paul Quinit <paulquinit@gmail.com>
2022-12-14 18:32:26 +0800

    Add generateWebhookSignature and verifyWebhookSignatures

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import GeneratorHelper from './Helpers/GeneratorHelper'
+import CryptoHelper from './Helpers/CryptoHelper'
 
 type Options = {
   apiVersion: string,
@@ -25,10 +26,22 @@ export default class Twala {
 
   // utils
   public utils = new class {
+    public generatorHelper = new GeneratorHelper()
+    public cryptoHelper = new CryptoHelper()
+
     public generateNonce() {
-      const generatorHelper = new GeneratorHelper()
-      const nonce = generatorHelper.generateNonce()
+      const nonce = this.generatorHelper.generateNonce()
       return nonce
+    }
+
+    public generateWebhookSignature(stringifiedRequestBody: string, key: string) {
+      const signature = this.cryptoHelper.signDataHMAC(stringifiedRequestBody, key)
+      return signature
+    }
+
+    public verifyWebhookSignatures(headerSignature: string, webhookSignature: string) {
+      const isVerified = this.cryptoHelper.verifySignatures(headerSignature, webhookSignature)
+      return isVerified
     }
   }
 }
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,10 +1,16 @@
 import Twala from '../src/index'
 
 test('generate nonce', () => {
-  const twala = new Twala('test_app_uuid', 'test_app_secret', {
-    apiVersion: 'v2',
-    maxNetworkRetries: 1,
-    timeout: 20000,
-  })
+  const twala = new Twala('test_app_uuid', 'test_app_secret')
   expect(twala.utils.generateNonce()).toMatch(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi)
+})
+
+test('generate webhook signature', () => {
+  const twala = new Twala('test_app_uuid', 'test_app_secret')
+  expect(typeof twala.utils.generateWebhookSignature('test', 'test')).toBe('string')
+})
+
+test('verify webhook signatures', () => {
+  const twala = new Twala('test_app_uuid', 'test_app_secret')
+  expect(typeof twala.utils.verifyWebhookSignatures('88cd2108b5347d973cf39cdf9053d7dd42704876d8c9a9bd8e2d168259d3ddf7', '88cd2108b5347d973cf39cdf9053d7dd42704876d8c9a9bd8e2d168259d3ddf7')).toBe('boolean')
 })
